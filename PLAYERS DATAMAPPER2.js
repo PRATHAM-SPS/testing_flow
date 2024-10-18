8ab4e44a2c7cd7f11148a5aeac6a1a3942a0281d
Pratham Ingawale <94799826+PRATHAM-SPS@users.noreply.github.com>
2024-10-18 18:07:43 +0530

    Add player data mapper function to the repository

---
+++ PLAYERS DATAMAPPER2.js
@@ -0,0 +1,31 @@
+function playerDataMapper(data) {
+  try {
+    return utils.merge(data, {
+      playerId: data.Person.Code,
+      personName: {
+        name: data.Person.Name,
+        alias: data.Person.Alias,
+        aliasRaw: data.Person.AliasRaw,
+        passportName: data.Person.PassportName,
+        passportSurname: data.Person.PassportSurname,
+      },
+      teamId: data.Club.Code,
+      teamName: data.Club.Name,
+      birthDate: data.Person.BirthDate,
+      jerseyNumber: data.Person.JerseyName,
+      height: data.Person.Height,
+      weight: data.Person.Weight,
+      active: true, // Assuming all players are active as per the provided data
+    });
+  } catch (error) {
+    app.jobs.logJob("playerDataMapper", "failed", {
+      data: data,
+      errorStack: error.stack || null,
+      error: error || null
+    });
+  }
+}
+
+module.exports = {
+  playerDataMapper
+};
